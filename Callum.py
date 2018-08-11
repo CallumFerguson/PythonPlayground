9a58f489290f37617854fc9743e92ddd200350f4
Callum Ferguson <Calxferg@gmail.com>
2018-08-11 00:00:08 -0500

    Callum

---
+++ Callum.py
@@ -0,0 +1,22 @@
+
+b = 5
+
+print(type(b))
+
+c = 5.1
+
+b = c * 4
+
+print(type(b))
+
+d = False
+
+print(3 / 2)
+
+mylist = ['ths is a symbole: " <--', "test", 5]
+
+print(len(mylist))
+
+for i in range(2):
+    print('yolo')
+
