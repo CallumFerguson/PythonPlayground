893941b7f4338e08569abc96cc815b4df082ebd5
Pranal <pranalmadria@gmail.com>
2018-08-12 00:03:42 -0500

    Pi is my favorite number

--- Pranal.py
+++ Pranal.py
@@ -1,17 +1,8 @@
-def changeInt(x, y, z):
-    x = 0
-    y = 0
-    z = 0
+def iteration(counter):
+    return((4 * counter**2) / (4 * counter**2 - 1))
 
-
-a = 3
-b = 2
-c = 9
-
-#
-print((a//b))
-
-colors = ['red', 1, 'green']
-
-print(len(colors))
-
+sum = 1
+for i in range (1, 10000):
+    sum = sum * iteration(i)
+sum = sum * 2
+print(sum)
